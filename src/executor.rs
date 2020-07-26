a30b40d1733195a264ebf04b20ddead295d93fe1
cssivision <cssivision@gmail.com>
2020-07-26 15:45:48 +0800

    add tick

--- src/executor.rs
+++ src/executor.rs
@@ -1,7 +1,8 @@
+use std::cell::Cell;
 use std::fmt;
 use std::future::Future;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 
 use concurrent_queue::ConcurrentQueue;
 use rand::Rng;
@@ -22,6 +23,10 @@ impl Executor {
                 queue: ConcurrentQueue::unbounded(),
                 notified: AtomicBool::new(false),
                 shards: RwLock::new(Vec::new()),
+                sleepers: Mutex::new(Sleepers {
+                    count: 0,
+                    callbacks: Vec::new(),
+                }),
             }),
         }
     }
@@ -47,6 +52,8 @@ impl Executor {
             global: Arc::new(self.global.clone()),
             shard: Arc::new(ConcurrentQueue::bounded(512)),
             callback: Callback::new(notify),
+            sleeping: Cell::new(false),
+            ticks: Cell::new(0),
         };
 
         self.global
@@ -73,6 +80,14 @@ impl Callback {
     }
 }
 
+impl PartialEq for Callback {
+    fn eq(&self, other: &Callback) -> bool {
+        Arc::ptr_eq(&self.0, &other.0)
+    }
+}
+
+impl Eq for Callback {}
+
 impl fmt::Debug for Callback {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("<callback>").finish()
@@ -91,6 +106,68 @@ struct Global {
 
     /// Shards of the global queue created by tickers.
     shards: RwLock<Vec<Arc<ConcurrentQueue<Runnable>>>>,
+
+    /// A list of sleeping tickers.
+    sleepers: Mutex<Sleepers>,
+}
+
+/// A list of sleeping tickers.
+#[derive(Debug)]
+struct Sleepers {
+    /// Number of sleeping tickers (both notified and unnotified).
+    count: usize,
+
+    /// Callbacks of sleeping unnotified tickers.
+    ///
+    /// A sleeping ticker is notified when its callback is missing from this list.
+    callbacks: Vec<Callback>,
+}
+
+impl Sleepers {
+    /// Returns notification callback for a sleeping ticker.
+    ///
+    /// If a ticker was notified already or there are no tickers, `None` will be returned.
+    fn notify(&mut self) -> Option<Callback> {
+        if self.callbacks.len() == self.count {
+            self.callbacks.pop()
+        } else {
+            None
+        }
+    }
+
+    /// Re-inserts a sleeping ticker's callback if it was notified.
+    ///
+    /// Returns `true` if the ticker was notified.
+    fn update(&mut self, callback: &Callback) -> bool {
+        if self.callbacks.iter().all(|cb| cb != callback) {
+            self.callbacks.push(callback.clone());
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Returns `true` if a sleeping ticker is notified or no tickers are sleeping.
+    fn is_notified(&self) -> bool {
+        self.count == 0 || self.count > self.callbacks.len()
+    }
+
+    /// Removes a previously inserted sleeping ticker.
+    fn remove(&mut self, callback: &Callback) {
+        self.count -= 1;
+        for i in (0..self.callbacks.len()).rev() {
+            if &self.callbacks[i] == callback {
+                self.callbacks.remove(i);
+                return;
+            }
+        }
+    }
+
+    /// Inserts a new sleeping ticker.
+    fn insert(&mut self, callback: &Callback) {
+        self.count += 1;
+        self.callbacks.push(callback.clone());
+    }
 }
 
 impl Global {
@@ -118,12 +195,88 @@ pub struct Ticker {
 
     /// Callback invoked to wake this ticker up.
     callback: Callback,
+
+    /// Set to `true` when in sleeping state.
+    ///
+    /// States a ticker can be in:
+    /// 1) Woken.
+    /// 2a) Sleeping and unnotified.
+    /// 2b) Sleeping and notified.
+    sleeping: Cell<bool>,
+
+    /// Bumped every time a task is run.
+    ticks: Cell<usize>,
 }
 
 impl Ticker {
+    /// Moves the ticker into sleeping and unnotified state.
+    ///
+    /// Returns `false` if the ticker was already sleeping and unnotified.
+    fn sleep(&self) -> bool {
+        let mut sleepers = self.global.sleepers.lock().unwrap();
+
+        if self.sleeping.get() {
+            // Already sleeping, check if notified.
+            if !sleepers.update(&self.callback) {
+                return false;
+            }
+        } else {
+            // Move to sleeping state.
+            sleepers.insert(&self.callback);
+        }
+
+        self.global
+            .notified
+            .swap(sleepers.is_notified(), Ordering::SeqCst);
+
+        self.sleeping.set(true);
+        true
+    }
+
+    fn wake(&self) -> bool {
+        if self.sleeping.get() {
+            let mut sleepers = self.global.sleepers.lock().unwrap();
+            sleepers.remove(&self.callback);
+
+            self.global
+                .notified
+                .swap(sleepers.is_notified(), Ordering::SeqCst);
+        }
+
+        self.sleeping.replace(false)
+    }
+
     /// Runs a single task and returns `true` if one was found.
     pub fn tick(&self) -> bool {
-        loop {}
+        loop {
+            match self.search() {
+                None => {
+                    if !self.sleep() {
+                        return false;
+                    }
+                }
+                Some(r) => {
+                    self.wake();
+
+                    // Notify another ticker now to pick up where this ticker left off, just in
+                    // case running the task takes a long time.
+                    self.global.notify();
+
+                    // Bump the ticker.
+                    let ticks = self.ticks.get();
+                    self.ticks.set(ticks.wrapping_add(1));
+
+                    // Steal tasks from the global queue to ensure fair task scheduling.
+                    if ticks % 64 == 0 {
+                        steal(&self.global.queue, &self.shard);
+                    }
+
+                    r.run();
+
+                    return true;
+                }
+            }
+        }
     }
 
     /// Finds the next task to run.
