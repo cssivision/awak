84216c6a19426601fd9233aea7982ce051a6712b
cssivision <cssivision@gmail.com>
2020-08-05 17:41:51 +0800

    source add writeable

--- src/async_io/reactor.rs
+++ src/async_io/reactor.rs
@@ -2,17 +2,19 @@ use std::io;
 use std::os::unix::io::RawFd;
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
-    Mutex,
+    Arc, Mutex,
 };
 use std::task::{Poll, Waker};
 
 use super::sys;
 
 use futures::future::poll_fn;
+use slab::Slab;
 
 pub struct Reactor {
     ticker: AtomicUsize,
     sys: sys::Reactor,
+    sources: Mutex<Slab<Arc<Source>>>,
 }
 
 impl Reactor {
@@ -20,6 +22,7 @@ impl Reactor {
         Reactor {
             ticker: AtomicUsize::new(0),
             sys: sys::Reactor::new().expect("init reactor fail"),
+            sources: Mutex::new(Slab::new()),
         }
     }
 
@@ -57,6 +60,12 @@ impl Source {
         poll_fn(|cx| {
             let mut w = self.wakers.lock().unwrap();
 
+            if let Some((a, b)) = ticks {
+                if w.tick_readable != a && w.tick_readable != b {
+                    return Poll::Ready(Ok(()));
+                }
+            }
+
             if w.readers.is_empty() {
                 // no readers, register in reactor
                 Reactor::get().interest(self.raw, self.key, true, !w.writers.is_empty())?;
@@ -70,6 +79,38 @@ impl Source {
                 ticks = Some((
                     Reactor::get().ticker.load(Ordering::SeqCst),
                     w.tick_readable,
+                ));
+            }
+            Poll::Pending
+        })
+        .await
+    }
+
+    pub async fn writeable(&self) -> io::Result<()> {
+        let mut ticks = None;
+
+        poll_fn(|cx| {
+            let mut w = self.wakers.lock().unwrap();
+
+            if let Some((a, b)) = ticks {
+                if w.tick_writeable != a && w.tick_writeable != b {
+                    return Poll::Ready(Ok(()));
+                }
+            }
+
+            if w.readers.is_empty() {
+                // no readers, register in reactor
+                Reactor::get().interest(self.raw, self.key, !w.readers.is_empty(), true)?;
+            }
+
+            if w.writers.iter().all(|w| !w.will_wake(cx.waker())) {
+                w.writers.push(cx.waker().clone());
+            }
+
+            if ticks.is_none() {
+                ticks = Some((
+                    Reactor::get().ticker.load(Ordering::SeqCst),
+                    w.tick_writeable,
                 ));
             }
             Poll::Pending
--- src/async_io/sys/mod.rs
+++ src/async_io/sys/mod.rs
@@ -1,3 +1,14 @@
+macro_rules! syscall {
+    ($fn:ident $args:tt) => {{
+        let res = unsafe { libc::$fn $args };
+        if res == -1 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            Ok(res)
+        }
+    }};
+}
+
 mod epoll;
 
 pub use epoll::*;
