97909025e16bd42c4695cba3f534aa27748dbf9a
cssivision <cssivision@gmail.com>
2020-08-12 12:32:57 +0800

    epoll reactor

--- src/async_io/sys/epoll.rs
+++ src/async_io/sys/epoll.rs
@@ -1,23 +1,122 @@
+use std::convert::TryInto;
 use std::io;
 use std::os::unix::io::RawFd;
+use std::ptr;
+use std::time::Duration;
 
-pub struct Reactor {}
+const NOTIFY_KEY: usize = usize::MAX;
+
+pub struct Reactor {
+    epoll_fd: RawFd,
+    event_fd: RawFd,
+}
+
+/// Epoll flags for all possible readability events.
+fn read_flags() -> libc::c_int {
+    libc::EPOLLIN | libc::EPOLLRDHUP | libc::EPOLLHUP | libc::EPOLLERR | libc::EPOLLPRI
+}
+
+/// Epoll flags for all possible writability events.
+fn write_flags() -> libc::c_int {
+    libc::EPOLLOUT | libc::EPOLLHUP | libc::EPOLLERR
+}
 
 impl Reactor {
     pub fn new() -> io::Result<Reactor> {
-        unimplemented!();
+        let epoll_fd = syscall!(epoll_create(1024))?;
+        let flags = syscall!(fcntl(epoll_fd, libc::F_GETFD))?;
+        syscall!(fcntl(epoll_fd, libc::F_SETFD, flags | libc::FD_CLOEXEC))?;
+
+        let event_fd = syscall!(eventfd(0, libc::EFD_CLOEXEC | libc::EFD_NONBLOCK))?;
+
+        let reactor = Reactor { epoll_fd, event_fd };
+
+        reactor.insert(event_fd)?;
+        Ok(reactor)
     }
 
     pub fn interest(&self, fd: RawFd, key: usize, read: bool, write: bool) -> io::Result<()> {
-        unimplemented!();
+        let mut flags = libc::EPOLLONESHOT;
+        if read {
+            flags |= read_flags();
+        }
+        if write {
+            flags |= write_flags();
+        }
+
+        let mut ev = libc::epoll_event {
+            events: flags as _,
+            u64: key as u64,
+        };
+
+        syscall!(epoll_ctl(self.epoll_fd, libc::EPOLL_CTL_MOD, fd, &mut ev))?;
+
+        Ok(())
     }
 
     pub fn insert(&self, fd: RawFd) -> io::Result<()> {
-        unimplemented!();
+        let flags = syscall!(fcntl(fd, libc::F_GETFD))?;
+        syscall!(fcntl(fd, libc::F_SETFD, flags | libc::O_NONBLOCK))?;
+
+        let mut ev = libc::epoll_event {
+            events: 0,
+            u64: 0u64,
+        };
+
+        syscall!(epoll_ctl(self.epoll_fd, libc::EPOLL_CTL_ADD, fd, &mut ev))?;
+        Ok(())
     }
 
     pub fn remove(&self, fd: RawFd) -> io::Result<()> {
-        unimplemented!();
+        syscall!(epoll_ctl(
+            self.epoll_fd,
+            libc::EPOLL_CTL_DEL,
+            fd,
+            ptr::null_mut()
+        ))?;
+
+        Ok(())
+    }
+
+    pub fn wait(&self, events: &mut Events, timeout: Option<Duration>) -> io::Result<usize> {
+        let timeout_ms = timeout
+            .map(|t| {
+                if t == Duration::from_millis(0) {
+                    t
+                } else {
+                    // Non-zero duration must be at least 1ms.
+                    t.max(Duration::from_millis(1))
+                }
+            })
+            .and_then(|t| t.as_millis().try_into().ok())
+            .unwrap_or(-1);
+
+        let res = syscall!(epoll_wait(
+            self.epoll_fd,
+            events.list.as_mut_ptr() as *mut libc::epoll_event,
+            events.list.len() as libc::c_int,
+            timeout_ms as libc::c_int,
+        ))?;
+
+        events.len = res as usize;
+
+        let mut buf = [0u8; 8];
+        let _ = syscall!(read(
+            self.event_fd,
+            &mut buf[0] as *mut u8 as *mut libc::c_void,
+            buf.len()
+        ));
+        self.interest(self.event_fd, NOTIFY_KEY, true, false)?;
+
+        Ok(events.len)
+    }
+}
+
+impl Drop for Reactor {
+    fn drop(&mut self) {
+        let _ = self.remove(self.event_fd);
+        let _ = syscall!(close(self.event_fd));
+        let _ = syscall!(close(self.epoll_fd));
     }
 }
 
