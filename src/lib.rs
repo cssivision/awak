02939a51ba5fd69f295f2d6803388253976e7ac0
cssivision <cssivision@gmail.com>
2020-08-24 19:52:49 +0800

    add Incoming

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-#[macro_use]
-mod pin;
+#[macro_export]
+macro_rules! pin_mut {
+    ($($x:ident),* $(,)?) => { $(
+        // Move the value to ensure that it is owned
+        let mut $x = $x;
+        // Shadow the original binding so that it can't be directly accessed
+        // ever again.
+        #[allow(unused_mut)]
+        let mut $x = unsafe {
+            std::pin::Pin::new_unchecked(&mut $x)
+        };
+    )* }
+}
+
+#[macro_export]
+macro_rules! ready {
+    ($e:expr $(,)?) => {
+        match $e {
+            std::task::Poll::Ready(t) => t,
+            std::task::Poll::Pending => return core::task::Poll::Pending,
+        }
+    };
+}
+
 mod blocking;
 mod executor;
 mod io;
--- src/net/tcp/listener.rs
+++ src/net/tcp/listener.rs
@@ -1,8 +1,13 @@
+use std::future::Future;
 use std::io;
 use std::net::{self, SocketAddr, ToSocketAddrs};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 use super::stream::TcpStream;
 use crate::io::Async;
+
+use futures::stream::Stream;
 
 pub struct TcpListener {
     inner: Async<net::TcpListener>,
@@ -21,5 +26,25 @@ impl TcpListener {
         let (stream, addr) = self.inner.read_with(|io| io.accept()).await?;
 
         Ok((TcpStream::new(stream)?, addr))
+    }
+
+    pub fn incoming(&self) -> Incoming<'_> {
+        Incoming { inner: self }
+    }
+}
+
+pub struct Incoming<'a> {
+    inner: &'a TcpListener,
+}
+
+impl<'a> Stream for Incoming<'a> {
+    type Item = io::Result<TcpStream>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let fut = self.inner.accept();
+        pin_mut!(fut);
+
+        let (stream, _) = ready!(fut.poll(cx))?;
+        Poll::Ready(Some(Ok(stream)))
     }
 }
