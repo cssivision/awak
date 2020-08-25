32220772cec16f06d08cddb0da3c789a29e81b0d
cssivision <cssivision@gmail.com>
2020-08-25 22:17:42 +0800

    example: spawn task

--- examples/tcp-listener.rs
+++ examples/tcp-listener.rs
@@ -10,10 +10,12 @@ fn main() {
             while let Some(stream) = listener.incoming().next().await {
                 match stream {
                     Ok(mut stream) => {
-                        let mut output = [0u8; 11];
-                        let _ = stream.read_exact(&mut output).await;
-                        println!("output: {:?}", String::from_utf8(output.to_vec()));
-                        let _ = stream.write_all(b"hello world").await;
+                        awak::spawn(async move {
+                            let mut output = [0u8; 11];
+                            let _ = stream.read_exact(&mut output).await;
+                            println!("output: {:?}", String::from_utf8(output.to_vec()));
+                            let _ = stream.write_all(b"hello world").await;
+                        });
                     }
                     Err(e) => {
                         println!("err: {}", e);
