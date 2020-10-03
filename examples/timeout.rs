980814642f75ff6497b354bd017baf74eb33bf92
cssivision <cssivision@gmail.com>
2020-10-03 14:36:18 +0800

    add timeout example

---
+++ examples/timeout.rs
@@ -0,0 +1,25 @@
+use std::time::Duration;
+
+use awak::io::AsyncReadExt;
+use awak::net::TcpStream;
+use awak::time::timeout;
+
+fn main() {
+    awak::block_on(async {
+        match timeout(Duration::from_secs(1), TcpStream::connect("127.0.0.1:8080")).await {
+            Ok(stream) => match stream {
+                Ok(mut stream) => {
+                    let mut buf = vec![0; 10];
+                    let n = stream.read_exact(&mut buf).await.unwrap();
+                    println!("read {:?} bytes", n);
+                }
+                Err(e) => {
+                    println!("connect err: {:?}", e);
+                }
+            },
+            Err(e) => {
+                println!("timeout err: {:?}", e);
+            }
+        }
+    });
+}
